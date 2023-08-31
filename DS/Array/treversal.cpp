e5efc92e28867d644a86c66a9e00ecbac2cb1594
Shahriar Hossain Tamim <108519950+TamimCS@users.noreply.github.com>
2023-08-31 20:37:22 +0600

    Update treversal.cpp

--- DS/Array/treversal.cpp
+++ DS/Array/treversal.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a[50],size;
+cout<<"Enter array size: ";
+cin>>size;
+if (size>50)
+{
+cout<<"Overflow";
+}else{
 
+for (int i = 1; i <= size; i++)
+{cout<<"Input Array element "<<i<<": ";
+    cin>>a[i];
+}
+cout<<"Showing array element : ";
+for (int i = 1; i <=size; i++)
+{
+    cout<<"\t"<<a[i];
+}}
+
+
+}
