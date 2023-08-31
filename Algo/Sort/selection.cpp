b909d4e348cb870bee4b8f5792e3fb52feb921a4
Shahriar Hossain Tamim <108519950+TamimCS@users.noreply.github.com>
2023-08-31 22:59:23 +0600

    Update selection.cpp

--- Algo/Sort/selection.cpp
+++ Algo/Sort/selection.cpp
@@ -1,49 +1,35 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-// Function for Selection sort
-void selectionSort(int arr[], int n)
-{
-    int i, j, min_idx;
- 
-    // One by one move boundary of
-    // unsorted subarray
-    for (i = 0; i < n - 1; i++) {
- 
-        // Find the minimum element in
-        // unsorted array
-        min_idx = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
+#include<iostream>
+ using namespace std;
+
+void sort(int arr[],int n){
+    for (int i = 0; i <n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[j]<arr[i])
+            {
+               int temp= arr[j];
+               arr[j]=arr[i];
+               arr[i]=temp;
+            }
+            
         }
- 
-        // Swap the found minimum element
-        // with the first element
-        if (min_idx != i)
-            swap(arr[min_idx], arr[i]);
+        
     }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+    
+
 }
- 
-// Function to print an array
-void printArray(int arr[], int size)
+int main(){
+int n;
+cin>>n;
+int arr[n];
+for (int i = 0; i < n; i++)
 {
-    int i;
-    for (i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-        cout << endl;
-    }
+    cin>>arr[i];
 }
- 
-// Driver program
-int main()
-{
-    int arr[] = { 64, 25, 12, 22, 11 };
-    int n = sizeof(arr) / sizeof(arr[0]);
- 
-    // Function Call
-    selectionSort(arr, n);
-    cout << "Sorted array: \n";
-    printArray(arr, n);
-    return 0;
+sort(arr,n);
 }
